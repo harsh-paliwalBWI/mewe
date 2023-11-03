5a66e5d2d7d57bd6d4a6c3293f4b3b6f4a3a1557
paliwalharsh <paliwalharsh2018@gmail.com>
2023-11-03 18:56:23 +0530

    issue

--- components/homepage/HomeComponent.tsx
+++ components/homepage/HomeComponent.tsx
@@ -23,12 +23,7 @@ const HomeComponent = () => {
         <Newsletter />
         <InviteBanner />
       </div>
-      <Link href={{ pathname: "/account", query: { tab: "chat" } }}>
-        <div className="fixed bottom-0 right-3 px-1 sm:px-2 md:px-4 lg:px-6 py-1 sm:py-2 md:py-3 bg-[#054A91] cursor-pointer  text-white rounded-t-xl w-[15%] flex justify-center gap-1 sm:gap-2 md:gap-3 z-40">
-          <h1 className="md:text-base sm:text-sm text-xs">CHAT</h1>
-          <FlatIcon className="flaticon-down-right-2 text-white text-xs sm:text-sm md:text-base font-bold drop-shadow-2xl" />
-        </div>
-      </Link>
+     
     </>
   );
 };
